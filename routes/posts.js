ad178834d13e849ef6e5de31a00f76cc0f610cd4
Wright & Tina Ramsey <student@epicodus.com>
2015-03-02 12:10:39 -0800

    add dynamic post listing

---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+Bustle.PostsRoute = Ember.Route.extend({
+  controllerName: 'posts',
+  renderTemplate: function() {
+    this.render('posts');
+  },
+  model: function() {
+    return posts;
+  }
+});
