375b7736487dfccefb1307ebf362cf5d315febcd
Dan Wright <muscle.crab.13@gmail.com>
2015-03-03 08:09:20 -0800

    add new carousel code

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,27 @@
 Bustle.PostsController = Ember.Controller.extend({
 
-  currentPostId: [1,0,0,0],
+  currentPost: 0,
+  imageTitle: "",
+
+  model: function() {
+    return posts;
+  },
 
   actions: {
-    dieEmberDie: function() {
-      set
-    }
+    carouselNext: function() {
+      this.set('currentPost', (this.currentPost + 1));
+      if (this.currentPost == 5) {
+        this.set('currentPost', 0);
+      }
+      this.set('imageTitle', posts[this.currentPost].image)
+    },
+
+    carouselPrev: function() {
+      if (this.currentPost == 0) {
+        this.set('currentPost', 5);
+      }
+      this.set('currentPost', (this.currentPost - 1));
+      this.set('imageTitle', posts[this.currentPost].image)
+    },
   }
 });
--- store.js
+++ store.js
@@ -8,8 +8,8 @@ var posts = [
 
   {
     id: '1',
-    image: 'bananas.jpg',
-    title: 'Space Confirmed As Cancer Causing Agend',
+    image: 'pizza.jpg',
+    title: 'Replacing Your Skin With Felt For Fun And Profit',
     body: 'Selfishness is born in the gap where potential has been excluded. Consciousness consists of pulses of quantum energy. “Quantum” means a summoning of the interstellar. This life is nothing short of a blossoming rebirth of self-aware non-locality. To roam the mission is to become one with it. The Law of Attraction may be the solution to what’s holding you back from an unimaginable rekindling of potentiality. You will soon be awakened by a power deep within yourself — a power that is spiritual, sacred. Through affirmations, our hearts are engulfed in potential. Humankind has nothing to lose. Reality has always been bursting with beings whose hearts are transformed into consciousness. We are in the midst of a sublime redefining of wellbeing that will remove the barriers to the totality itself. If you have never experienced this flow at the quantum level, it can be difficult to vibrate. We are at a crossroads of power and discontinuity. Our conversations with other beings have led to a deepening of hyper-powerful consciousness. Throughout history, humans have been interacting with the biosphere via morphic resonance. It is a sign of things to come. Imagine an unfolding of what could be. This circuit never ends.'
   },
 
@@ -42,7 +42,7 @@ var posts = [
   },
 
   {
-    id: '5',
+    id: '6',
     image: 'dinosaur.jpg',
     title: 'One Weird Old Trick To Get Healthier Skin And Larger Muscles!',
     body: 'You must take a stand against suffering. It can be difficult to know where to begin. Wanderer, look within and synergize yourself. Although you may not realize it, you are pranic. We must learn how to lead self-aware lives in the face of ego. The osmosis of awareness is now happening worldwide. The future will be an ethereal ennobling of inseparability. As you exist, you will enter into infinite empathy that transcends understanding. Through feng shui, our bodies are immersed in inspiration. Alternative medicine may be the solution to what’s holding you back from a powerful reimagining of rebirth. How should you navigate this sentient quantum cycle? You will soon be recreated by a power deep within yourself — a power that is quantum, high-frequency. You may be ruled by ego without realizing it. Do not let it obliterate the nature of your journey. The complexity of the present time seems to demand an unveiling of our lives if we are going to survive. Have you found your myth? The biosphere is calling to you via supercharged electrons. Can you hear it? If you have never experienced this paradigm shift of the creative act, it can be difficult to dream.'
